d25a39b06206cc4e95fc9529ce5d6843bfc29677
MsMmD <mateus.scheffer@usp.br>
2026-05-17 01:35:40 -0300

    initializing project

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"-------------------------------------\nBig primes generator with uncertainty\n-------------------------------------\n";
+    return 0;
+}
